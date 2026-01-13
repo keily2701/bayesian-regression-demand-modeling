467eaaace64d53ca1bb2017e67ec3e2e16042bba
Keily Oropeza <keilyoropeza270119@gmail.com>
2026-01-13 18:43:45 -0300

    Update load_data.R

--- src/load_data.R
+++ src/load_data.R
@@ -1,7 +1,10 @@
 # load_data.R
 # Functions for loading and preparing outbreak data
 
-load_outbreak_data <- function(path, sep = ";") {
-  data <- read.table(path, sep = sep, header = TRUE)
-  return(data)
+cargar_datos <- function(ruta_csv){
+  datos <- read.table(ruta_csv, sep = ";", header = TRUE)
+  dato1 <- data.matrix(datos)
+  variables <- colnames(dato1)
+  
+  return(list(dato1 = dato1, variables = variables))
 }
