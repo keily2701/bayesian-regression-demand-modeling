33f11b660989c8808bc40617f50b06c39876afba
Keily Oropeza <keilyoropeza270119@gmail.com>
2026-02-11 21:20:09 -0300

    Update glm_models.R

--- src/glm_models.R
+++ src/glm_models.R
@@ -1,17 +1,13 @@
 # glm_models.R
 # Modelos lineales generalizados
 
-fit_poisson_glm <- function(data, response, exclude_vars = NULL) {
-  df <- data.frame(data)
-  
-  if (!is.null(exclude_vars)) {
-    formula <- as.formula(
-      paste(response, "~ . -", paste(exclude_vars, collapse = " - "))
-    )
-  } else {
-    formula <- as.formula(paste(response, "~ ."))
-  }
-  
-  glm(formula, data = df, family = poisson)
+
+fit_glm_poisson <- function(formula, train_data) {
+  glm(formula, data = train_data, family = poisson)
+}
+
+# Funcion de prediccion
+predict_glm <- function(model, test_data) {
+  predict(model, newdata = test_data, type = "response")
 }
 
